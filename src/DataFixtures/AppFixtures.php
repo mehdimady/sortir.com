eef092edf0b08dc42b27e393f0cf1dd51622ffd5
Mehdi MADY <mehdimady@gmail.Com>
2022-05-31 00:18:13 +0200

    Création des entitées / Installation Apache-pack

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ville;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker =Faker\Factory::create('fr_FR');
+        $villes=[];
+        for ($i=0 ; $i <10 ; $i++){
+            $ville = new Ville();
+            $ville->setNom($faker->city());
+            $ville->setCodePostal($faker->randomNumber(5,true));
+            $manager->persist($ville);
+            $villes[]=$ville;
+        }
+
+
+        $manager->flush();
+    }
+}
