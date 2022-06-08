fa33438033973b3b1ce4137d9f49e7b9ff975a00
sofianechaghla <77396241+sofianechaghla@users.noreply.github.com>
2022-06-08 16:54:17 +0200

    css sofiane

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -9,6 +9,10 @@ use App\Entity\Sortie;
 use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -26,12 +30,45 @@ class SortieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('dateHeureDebut')
-            ->add('dateLimiteInscription')
-            ->add('duree')
-            ->add('nbInscriptionsMax')
-            ->add('infosSortie')
+            ->add('nom',TextType::class,[
+                'attr'=>[
+                    'class'=>'form-control'
+                ]
+            ])
+
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'label' => 'Entre le',
+                'widget' => 'single_text',
+                'html5' => true,
+                'required'=>false,
+                'attr'=>[
+                    'class'=>'form-control'
+                ],
+            ])
+            ->add('dateLimiteInscription', DateTimeType::class, [
+                'label' => 'Entre le',
+                'widget' => 'single_text',
+                'html5' => true,
+                'required'=>false,
+                'attr'=>[
+                    'class'=>'form-control'
+                ],
+            ])
+            ->add('duree',NumberType::class,[
+            'attr'=>[
+         'class'=>'form-control'
+    ]
+            ])
+            ->add('nbInscriptionsMax',NumberType::class,[
+            'attr'=>[
+        'class'=>'form-control'
+    ]
+            ])
+            ->add('infosSortie',TextareaType::class,[
+            'attr'=>[
+        'class'=>'form-control'
+    ]
+            ])
             ->add('campus', TextType::class, [
                 'disabled' => true,
                 'mapped' => false,
