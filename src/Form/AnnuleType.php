3ad89666609e92195a0755014eb74bb300194bf4
Jokdeve <ramos.georges91070@gmail.com>
2022-06-09 18:49:07 +0200

    Maj

--- src/Form/AnnuleType.php
+++ src/Form/AnnuleType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Sortie;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,10 +14,11 @@ class AnnuleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('motif',TextType::class,[
+            ->add('motif',TextareaType::class,[
                 'label'=>'Motif',
                 'attr'=>[
-                    'class'=>'form-control'
+                    'class'=>'form-control',
+                    'rows'=>'5'
                 ],
             ])
         ;
